07030dd5ba04b17d55068682b624f802418c86e5
Ari Suutari <ari@stonepile.fi>
2014-09-18 19:49:51 +0300

    Add 'const' to tables to place them into flash. This saves about 200 bytes of ram.

--- common/crcutil.c
+++ common/crcutil.c
@@ -33,8 +33,8 @@
 // Local global variables
 ushort utilcrc16[MAX_PORTNUM];
 uchar utilcrc8[MAX_PORTNUM];
-static short oddparity[16] = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
-static uchar dscrc_table[] = {
+static const short oddparity[16] = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
+static const uchar dscrc_table[] = {
         0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
       157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
        35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
